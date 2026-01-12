d2093d50a37f762919866020b1f3d669a1b8deb8
Ioannis-FE <ioannisdimitriou@shipitsmarter.com>
2026-01-12 10:43:02 +0100

    extend use cases compose

--- src/application/compose/shows/useCases.ts
+++ src/application/compose/shows/useCases.ts
@@ -2,12 +2,17 @@ import { filterCrimeShowsUseCase } from "@/application/filters/useCases/filterCr
 import { filterPopularShowsUseCase } from "@/application/filters/useCases/filterPopularShowsUseCase"
 import { filterShowsUseCase } from "@/application/filters/useCases/filterShowsUseCase"
 import { getShowsUseCase } from "@/application/shows/useCases/getShowsUseCase"
-import { ShowRepository } from "@/infrastructure/show/repositories/showRepository"
+import { ShowRepository } from "@/infrastructure/shows/repositories/showRepository"
 import { searchShowsUsecase } from "@/application/shows/useCases/searchShowsUsecase"
 import {getShowsById} from "@/application/shows/useCases/getShowById"
+import { LocalStorageFavouriteRepository } from "@/infrastructure/favourites/repositories/LocalStorageFavouriteRepository"
+import { addFavouritesUseCase } from "@/application/favourites/useCases/addFavouritesUseCase"
+import { findAllFavouritesUseCase } from "@/application/favourites/useCases/findAllFavouritesUseCase"
 
 export const setupUseCases = () => {
   const repository = new ShowRepository()
+  const favouriteRepository = LocalStorageFavouriteRepository()
+
 
   return {
     getShows: getShowsUseCase(repository),
@@ -16,5 +21,7 @@ export const setupUseCases = () => {
     filterCrime: filterCrimeShowsUseCase,
     filterPopular: filterPopularShowsUseCase,
     filterShows: filterShowsUseCase,
+    addFavourites: addFavouritesUseCase(favouriteRepository),
+    findAllFavourites: findAllFavouritesUseCase(favouriteRepository),
   }
 }

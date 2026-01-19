fa46502c2002caa4b88076c90691b25c26ee664c
Ioannis-FE <ioannisdimitriou@shipitsmarter.com>
2026-01-19 14:49:41 +0100

    extend usecases

--- src/application/compose/shows/useCases.ts
+++ src/application/compose/shows/useCases.ts
@@ -1,13 +1,14 @@
-import { filterCrimeShowsUseCase } from "@/application/filters/useCases/filterCrimeShowsUseCase"
-import { filterPopularShowsUseCase } from "@/application/filters/useCases/filterPopularShowsUseCase"
-import { filterShowsUseCase } from "@/application/filters/useCases/filterShowsUseCase"
-import { getShowsUseCase } from "@/application/shows/useCases/getShowsUseCase"
+import { filterCrimeShowsUseCase } from "@/application/useCases/filters/filterCrimeShowsUseCase"
+import { filterPopularShowsUseCase } from "@/application/useCases/filters/filterPopularShowsUseCase"
+import { filterShowsUseCase } from "@/application/useCases/filters/filterShowsUseCase"
+import { getShowsUseCase } from "@/application/useCases/shows/getShowsUseCase"
 import { ShowRepository } from "@/infrastructure/shows/repositories/showRepository"
-import { searchShowsUsecase } from "@/application/shows/useCases/searchShowsUsecase"
-import {getShowsById} from "@/application/shows/useCases/getShowById"
+import { searchShowsUsecase } from "@/application/useCases/shows/searchShowsUseCase"
+import {getShowsById} from "@/application/useCases/shows/getShowByIdUseCase"
 import { LocalStorageFavouriteRepository } from "@/infrastructure/favourites/repositories/LocalStorageFavouriteRepository"
-import { addFavouritesUseCase } from "@/application/favourites/useCases/addFavouritesUseCase"
-import { findAllFavouritesUseCase } from "@/application/favourites/useCases/findAllFavouritesUseCase"
+import { addFavouritesUseCase } from "@/application/useCases/favourites/addFavouritesUseCase"
+import { findAllFavouritesUseCase } from "@/application/useCases/favourites/findAllFavouritesUseCase"
+import { getGenresUseCase } from "@/application/useCases/genres/getGenresUseCase"
 
 export const setupUseCases = () => {
   const repository = new ShowRepository()
@@ -23,5 +24,6 @@ export const setupUseCases = () => {
     filterShows: filterShowsUseCase,
     addFavourites: addFavouritesUseCase(favouriteRepository),
     findAllFavourites: findAllFavouritesUseCase(favouriteRepository),
+    getGenresUseCase: getGenresUseCase,
   }
 }
